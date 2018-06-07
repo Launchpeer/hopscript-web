2c2d6d66510ad2d8c774fbef2d07bd2a42df0b98
Kayla Kantola <krkantola@gmail.com>
2018-06-07 13:50:49 -0400

    makes onclick a passed down prop for leadgroup list item

--- src/components/Leads/LeadGroupList/LeadGroupList.js
+++ src/components/Leads/LeadGroupList/LeadGroupList.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../../UserActions';
 import { LeadGroupListItem } from './';
+import { removeLeadGroup } from './LeadGroupListActions';
 
 class LeadGroupList extends Component {
   constructor(props) {
     super(props);
     this.props.fetchUser();
+    this.handleRemoveLeadGroup = this.handleRemoveLeadGroup.bind(this);
   }
+
+  handleRemoveLeadGroup(id) {
+    this.props.removeLeadGroup(id);
+  }
+
   render() {
     const { leadGroups } = this.props.user.attributes;
     return (
       <div className="w-100">
         {leadGroups &&
           leadGroups.map(group => (
-            <LeadGroupListItem leadGroup={group} key={group.id} />
+            <LeadGroupListItem leadGroup={group} key={group.id} onClick={() => this.handleRemoveLeadGroup(group.id)} />
           ))}
       </div>
     );
@@ -28,7 +35,7 @@ const mapStateToProps = ({ UserReducer }) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchUser })(LeadGroupList);
+export default connect(mapStateToProps, { fetchUser, removeLeadGroup })(LeadGroupList);
 
 
 /*
