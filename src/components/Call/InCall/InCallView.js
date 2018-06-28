fc5f39fec3c55d2e66041cc0fdd8d09bc690179d
Lucius <lucius@Luciuss-iMac.local>
2018-06-28 11:46:50 -0400

    #42 adds routing to in-call view, and ability to refresh

--- src/components/Call/InCall/InCallView.js
+++ src/components/Call/InCall/InCallView.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { ArrowLeftCircle } from 'react-feather';
 import { Colors } from '../../../config/styles';
 import { CardRight, HSButton } from '../../common';
+import { fetchCall } from '../CallActions';
 
 class InCallView extends Component {
   constructor(props) {
@@ -9,58 +11,73 @@ class InCallView extends Component {
     this.state = {
       notes: true
     };
+    if (!this.props.currentCall) {
+      this.props.fetchCall(this.props.params.id);
+    }
   }
 
 
   render() {
+    const { currentCall } = this.props;
     const { notes } = this.state;
     const notesStyle = notes ? { color: Colors.brandPrimary, borderColor: Colors.brandPrimary } : { color: Colors.black, borderColor: Colors.lightGray };
     const questionsStyle = !notes ? { color: Colors.brandPrimary, borderColor: Colors.brandPrimary } : { color: Colors.black, borderColor: Colors.lightGray };
 
     return (
       <CardRight>
-        <div className="flex flex-row justify-between bb bw1 b--light-gray pa3 f4 mt2">
-          <div className="flex flex-row items-center">
-            <div role="button" className="pointer" onClick={() => console.log('this will nav back to script list')}>
-              <ArrowLeftCircle />
-            </div>
-            <div className="pl3 b">Script Name Here</div>
-          </div>
-          <div className="flex flex-column items-end pr4">
-            <div className="b f5 mb2">Lead Name</div>
-            <div className="silver f5">(555) 555 5555</div>
-          </div>
-        </div>
-        <div className="w-100 mt4 flex flex-row">
-
-          <div className="flex flex-column ml4 mr5 mv4 w-40 pa2">
-            <div className="pb2">
-              <div className="flex flex-row">
-                <div style={notesStyle} className="pointer b bb w4 tc pb2 bw2 f4" role="button" onClick={() => this.setState({ notes: true })}>
-              Notes
-                </div>
-                <div style={questionsStyle} className="pointer b bb w4 tc pb2 bw2 f4" role="button" onClick={() => this.setState({ notes: false })}>
-              Questions
-                </div>
+        {currentCall &&
+          <div>
+            <div className="flex flex-row justify-between bb bw1 b--light-gray pa3 f4 mt2">
+              <div className="flex flex-row items-center">
+                <div className="pl3 b">{currentCall.attributes.title}</div>
+              </div>
+              <div className="flex flex-column items-end pr4">
+                <div className="b f5 mb2">{currentCall.attributes.lead && currentCall.attributes.lead.attributes.name}</div>
+                <div className="silver f5">{currentCall.attributes.lead && currentCall.attributes.lead.attributes.phone}</div>
               </div>
             </div>
-            {notes &&
-            <div>
-              notes
-            </div>
-           }
-          </div>
-          <div className="mr5 mv4 w-60">
-          Question, audio player, answers
-          </div>
+            <div className="w-100 mt4 flex flex-row">
 
-        </div>
-        <div className="mr5 mb4">
-          <HSButton backgroundColor={Colors.brandRed} onClick={() => console.log('This will end the call')}>End Call</HSButton>
-        </div>
+              <div className="flex flex-column ml4 mr5 mv4 w-40 pa2">
+                <div className="pb2">
+                  <div className="flex flex-row">
+                    <div style={notesStyle} className="pointer b bb w4 tc pb2 bw2 f4" role="button" onClick={() => this.setState({ notes: true })}>
+                  Notes
+                    </div>
+                    <div style={questionsStyle} className="pointer b bb w4 tc pb2 bw2 f4" role="button" onClick={() => this.setState({ notes: false })}>
+                  Questions
+                    </div>
+                  </div>
+                </div>
+                {notes &&
+                <div>
+                  notes
+                </div>
+               }
+              </div>
+              <div className="mr5 mv4 w-60">
+              Question, audio player, answers
+              </div>
+
+            </div>
+            <div className="mr5 mb4">
+              <HSButton backgroundColor={Colors.brandRed} onClick={() => console.log('This will end the call')}>End Call</HSButton>
+            </div>
+          </div>
+        }
       </CardRight>
     );
   }
 }
 
-export default InCallView;
+
+const mapStateToProps = ({ CallReducer }) => {
+  const { loading, currentCall } = CallReducer;
+  console.log('currentCall', currentCall);
+  return {
+    loading,
+    currentCall
+  }
+}
+
+export default connect(mapStateToProps, { fetchCall })(InCallView);
