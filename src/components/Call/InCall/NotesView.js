fb6bf2fad6723ea2f1208750783f2727bbaabaab
Kayla Kantola <krkantola@gmail.com>
2018-06-29 18:48:24 -0400

    allows user to save notes to call object

--- src/components/Call/InCall/NotesView.js
+++ src/components/Call/InCall/NotesView.js
@@ -1,27 +1,68 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import ReactQuill from 'react-quill';
-
+import { saveNotes } from '../CallActions';
 
 class NotesView extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' }; // You can also pass a Quill Delta here
+    this.state = { text: '', saved: null };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(value) {
     this.setState({ text: value });
   }
 
+  // saves the notes and tells the user when the last save occured.
+  handleClick(currentCall, text) {
+    const timeAdjusted = (hours, minutes) => {
+      if (hours > 12) {
+        return (`${hours - 12}:${minutes}pm`);
+      }
+      return (`${hours}:${minutes}am`);
+    };
+    const currentTime = new Date();
+    const time = timeAdjusted(currentTime.getHours(), currentTime.getMinutes());
+    let thistext = text;
+    thistext = thistext.split('<p>').join('').split('</p>').join('');
+    this.props.saveNotes(currentCall, thistext);
+    this.setState({ saved: time });
+  }
+
   render() {
+    const { currentCall } = this.props;
+    const { saved } = this.state;
     return (
-      <ReactQuill
-        theme="bubble"
-        value={this.state.text}
-        placeholder="Write your notes here."
-        onChange={this.handleChange} />
+      <div>
+        <ReactQuill
+          theme="bubble"
+          value={this.state.text}
+          placeholder="Write your notes here."
+          onChange={this.handleChange} />
+        <div className="flex flex-row">
+          <div role="button"
+            className="brand-primary underline pointer hov-transparent"
+            onClick={() => this.handleClick(currentCall.id, this.state.text)}>
+          Save notes
+          </div>
+          {saved &&
+          <div className="pl2 moon-gray i">Last saved at {saved}</div>}
+        </div>
+      </div>
     );
   }
 }
 
-export default NotesView;
+
+const mapStateToProps = ({ CallReducer }) => {
+  const { currentCall, notes } = CallReducer;
+  return {
+    currentCall, notes
+  };
+};
+
+export default connect(mapStateToProps, {
+  saveNotes
+})(NotesView);
