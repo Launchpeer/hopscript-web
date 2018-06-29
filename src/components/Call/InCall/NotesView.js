40a3199f9cb186d2167fc3fa11a7b66a6536796e
Kayla Kantola <krkantola@gmail.com>
2018-06-29 17:17:50 -0400

    imports and formats react quill

--- src/components/Call/InCall/NotesView.js
+++ src/components/Call/InCall/NotesView.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactQuill from 'react-quill';
 
-const NotesView = () => (
-  <div>Notes</div>
-)
+
+class NotesView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' }; // You can also pass a Quill Delta here
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    this.setState({ text: value });
+  }
+
+  render() {
+    return (
+      <ReactQuill
+        theme="bubble"
+        value={this.state.text}
+        placeholder="Write your notes here."
+        onChange={this.handleChange} />
+    );
+  }
+}
 
 export default NotesView;
