35c4faabbda3368efbd780aaa219d977f0cb3ff8
Mary <marymhart@gmail.com>
2018-07-11 15:21:56 -0400

    closes #125 by increasing container size to 95% from 90

--- src/components/common/CardRight.js
+++ src/components/common/CardRight.js
@@ -4,7 +4,7 @@ import { FullScreenContainer, LoaderOrThis, HSCard } from './';
 const CardRight = props => (
   <FullScreenContainer>
     <LoaderOrThis loading={props.loading}>
-      <div className="w-90 absolute right-0 mb4" styles={{ paddingLeft: '100px' }}>
+      <div className="w-95 ml4 absolute right-1 mb4" styles={{ paddingLeft: '100px' }}>
         <HSCard>
           {props.children}
         </HSCard>
