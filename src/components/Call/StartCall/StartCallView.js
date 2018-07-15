14ef60ad345e7cb55860442a79282022d7a0b37d
Kayla Kantola <krkantola@gmail.com>
2018-07-15 08:34:16 -0400

    resolves #168

--- src/components/Call/StartCall/StartCallView.js
+++ src/components/Call/StartCall/StartCallView.js
@@ -10,8 +10,7 @@ import {
   HalfGrid,
   HSButton,
   CardRight,
-  HSCardHeader,
-  LoaderOrThis
+  HSCardHeader
 } from '../../common';
 import { SelectGroup, SelectLead, SelectScript, CallTitle } from './';
 import { fetchLeads, fetchLeadGroups } from '../../Leads/LeadsActions';
@@ -51,43 +50,41 @@ class StartCallView extends Component {
     } = this.props;
     return (
       <CardRight>
-        <LoaderOrThis loading={loading}>
-          <HSCardHeader>Start a Call</HSCardHeader>
-          <div className="pa3 mt4">
-            <form onSubmit={handleSubmit(this.handleFormSubmit)}>
-              <HalfGrid classOverrides="pr3">
-                <SelectGroup
-                  leadGroups={leadGroups}
-                  selectedGroup={this.state.selectedGroup}
-                  onClick={() => { this.setState({ selectedGroup: true, lead: null }); change('lead', null); }}
-                  classOverrides={!this.state.selectedGroup ? 'moon-gray' : 'brand-near-black'} />
-                <SelectLead
-                  leads={leads}
-                  selectedGroup={!this.state.selectedGroup}
-                  leadLoaded={this.state.lead}
-                  onClick={() => { this.setState({ selectedGroup: false }); change('leadGroup', null); }}
-                  selectLead={(lead) => { change('lead', lead); this.setState({ lead }); }}
-                  removeLead={() => { change('lead', null); this.setState({ lead: null }); }}
-                  classOverrides={this.state.selectedGroup ? 'moon-gray' : 'brand-near-black'} />
-              </HalfGrid>
-              <HalfGrid classOverrides="pl3 mb4">
-                {scripts.length > 0 && <SelectScript scripts={scripts} />}
-                <CallTitle />
-              </HalfGrid>
-              {error
+        <HSCardHeader>Start a Call</HSCardHeader>
+        <div className="pa3 mt4">
+          <form onSubmit={handleSubmit(this.handleFormSubmit)}>
+            <HalfGrid classOverrides="pr3">
+              <SelectGroup
+                leadGroups={leadGroups}
+                selectedGroup={this.state.selectedGroup}
+                onClick={() => { this.setState({ selectedGroup: true, lead: null }); change('lead', null); }}
+                classOverrides={!this.state.selectedGroup ? 'moon-gray' : 'brand-near-black'} />
+              <SelectLead
+                leads={leads}
+                selectedGroup={!this.state.selectedGroup}
+                leadLoaded={this.state.lead}
+                onClick={() => { this.setState({ selectedGroup: false }); change('leadGroup', null); }}
+                selectLead={(lead) => { change('lead', lead); this.setState({ lead }); }}
+                removeLead={() => { change('lead', null); this.setState({ lead: null }); }}
+                classOverrides={this.state.selectedGroup ? 'moon-gray' : 'brand-near-black'} />
+            </HalfGrid>
+            <HalfGrid classOverrides="pl3 mb4">
+              {scripts.length > 0 && <SelectScript scripts={scripts} />}
+              <CallTitle />
+            </HalfGrid>
+            {error
                 ?
                   <HSButton onClick={(e) => { e.preventDefault(); this.setState({ showError: true }); }}>Start Call</HSButton>
                 :
                   <HSButton>Start Call</HSButton>
               }
-              {this.state.showError && error &&
-                <div className="pa2">
-                  <RenderAlert error={{ message: error }} />
-                </div>
+            {this.state.showError && error &&
+            <div className="pa2">
+              <RenderAlert error={{ message: error }} />
+            </div>
               }
-            </form>
-          </div>
-        </LoaderOrThis>
+          </form>
+        </div>
       </CardRight>
     );
   }
