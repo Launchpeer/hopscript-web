8ea87a54c7c294b4635beb531e582ed01a8481f1
Kayla Kantola <krkantola@gmail.com>
2018-07-16 11:03:33 -0400

    displays formatting of question description in call view

--- src/components/History/HistoryListView.js
+++ src/components/History/HistoryListView.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
-import { HSButton, HSCardHeader, CardRight, LoaderOrThis } from '../common';
+import { HSCardHeader, CardRight, LoaderOrThis } from '../common';
 import { fetchHistory } from './HistoryActions';
 import { HistoryListItem } from './';
 
