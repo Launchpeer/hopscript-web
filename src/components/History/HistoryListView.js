0b5777085974aeb1547614d45db7555d90590131
Kayla Kantola <krkantola@gmail.com>
2018-08-15 15:35:33 -0400

    fixes history bug

--- src/components/History/HistoryListView.js
+++ src/components/History/HistoryListView.js
@@ -5,29 +5,37 @@ import { fetchHistory } from './HistoryActions';
 import { HistoryListItem } from './';
 
 class HistoryListView extends Component {
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchHistory();
   }
 
 
   render() {
     const { historyItems, loading } = this.props;
+    console.log('historyitems', historyItems);
     return (
       <CardRight>
         <HSCardHeader>Call History</HSCardHeader>
         <LoaderOrThis loading={loading}>
           <div className="ph3 pt4 pb3">
             <div className="w-100">
-              {historyItems && historyItems.length > 0 ?
-                <div className="w-100 mb5">
-                  {historyItems.map(historyItem => <HistoryListItem historyItem={historyItem} key={historyItem.id} />)}
-                </div> :
-                <div className="mt6 tc f4 pa3 silver">
-                  <div className="mb6">
-                You currently do not have any history <br />
-                Navigate to Scripts to get started! <br /> <br />
+              {historyItems && historyItems.length > 0
+                ?
+                  <div className="w-100 mb5">
+                    {historyItems.map((historyItem) => {
+                      if (historyItem.attributes.lead) {
+                      return <HistoryListItem historyItem={historyItem} key={historyItem.id} />;
+}
+                     })}
                   </div>
-                </div>}
+                :
+                  <div className="mt6 tc f4 pa3 silver">
+                    <div className="mb6">
+                  You currently do not have any history <br />
+                  Navigate to Scripts to get started! <br /> <br />
+                    </div>
+                  </div>
+              }
             </div>
           </div>
         </LoaderOrThis>
